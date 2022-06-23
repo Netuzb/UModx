dd73034cde439458afe2bedf9871e3127733c0df
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-06-24 00:47:18 +0500

    Update inline_stuff.py

--- hikka/modules/inline_stuff.py
+++ hikka/modules/inline_stuff.py
@@ -47,7 +47,7 @@ class InlineStuffMod(loader.Module):
             not getattr(message, "out", False)
             or not getattr(message, "via_bot_id", False)
             or message.via_bot_id != self.inline.bot_id
-            or "premium gallery..." not in getattr(message, "raw_text", "")
+            or "UModx galereya..." not in getattr(message, "raw_text", "")
         ):
             return
 
@@ -55,7 +55,7 @@ class InlineStuffMod(loader.Module):
 
         await message.delete()
 
-        m = await message.respond("<b>◽ premium gallery...</b>")
+        m = await message.respond("🌄 <b>UModx galereya...</b>")
 
         await self.inline.gallery(
             message=m,
