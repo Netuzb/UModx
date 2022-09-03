0b47676639916f9730ef1dc3a5ddf79e06a0549e
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-09-03 09:19:05 +0500

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -88,7 +88,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/hikariatama/Hikka",
+                "https://github.com/Netuzb/UModx",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
